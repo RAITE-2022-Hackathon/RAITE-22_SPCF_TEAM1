87ef0672d42dc17f0ae812ea741f80f11e9b8e6f
Vandre Suzuqui <78853050+vanguilard@users.noreply.github.com>
2022-09-30 23:43:03 +0800

    :100: Initial Files

---
+++ resources/js/App.vue
@@ -0,0 +1,12 @@
+<template>
+	<h1 class="text-primary">Laravel + Vue</h1>
+</template>
+
+<script>
+export default {
+	name: 'VueApp'
+}
+</script>
+
+<style>
+</style>
---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+// Import our custom CSS
+import '../sass/app.scss';
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+import { createApp } from 'vue';
+import app from './App.vue'
+
+createApp(app).mount("#app");
